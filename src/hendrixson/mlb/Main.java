6c7b8808fbfbbb09dee2868c82afa408dc606ea5
ryan <ryan.hendrixson@gmail.com>
2017-05-05 16:40:11 -0400

    messing around

--- src/hendrixson/mlb/Main.java
+++ src/hendrixson/mlb/Main.java
@@ -1,8 +1,6 @@
 package hendrixson.mlb;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.net.URL;
 
 public class Main {
@@ -11,7 +9,21 @@ public class Main {
         URL baseballLinscoreUrl = new URL("http://gd2.mlb.com/components/game/mlb/year_2017/month_05/day_05/gid_2017_05_05_nyamlb_chnmlb_1/linescore.json");
         InputStream is = baseballLinscoreUrl.openStream();
         try {
-            BufferedReader br = new BufferedReader(is);
+            BufferedReader br = new BufferedReader(new InputStreamReader(is));
+            String jsonString = readAll(br);
+            System.out.println(jsonString);
+        } catch (Exception e) {
+            System.err.println(e);
+            e.printStackTrace();
         }
+    }
+
+    private static String readAll(Reader rd) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        int cp;
+        while ((cp = rd.read()) != -1) {
+            sb.append((char) cp);
+        }
+        return sb.toString();
     }
 }
